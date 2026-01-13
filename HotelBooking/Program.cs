83cff4895151350ba06399bcb07c51e8dde81e12
koberidzeelene <koberidzeelene80@gmail.com>
2026-01-13 23:03:08 +0400

    .

--- HotelBooking/Program.cs
+++ HotelBooking/Program.cs
@@ -1,2 +1,61 @@
 ﻿
+using HotelBooking.Data;
+using HotelBooking.Models;
+using HotelBooking.Services;
+using Microsoft.EntityFrameworkCore;
+
 Console.WriteLine("hello world");
+
+
+var usersService = new UsersService();
+
+
+var existingUser = await usersService.GetUsersAsync();
+if (!existingUser.Any(u => u.Email == "elene3@mail.com"))
+{
+    await usersService.CreateUserAsync(new User
+    {
+        Username = "ele",
+        Email = "elene3@mail.com",
+        PasswordHash = "hashedpassword",
+        IsActive = true
+    });
+}
+
+
+var hotelsService = new HotelsService();
+
+await hotelsService.CreateHotelAsync(new Hotel
+{
+    Name = "Sun Hotel",
+    City = "Tbilisi",
+    Address = "Rustaveli Ave 10",
+    Description = "Luxury hotel in the city center",
+    Rating = 4.5m
+});
+
+await hotelsService.CreateHotelAsync(new Hotel
+{
+    Name = "Sea View",
+    City = "Batumi",
+    Address = "Beach Road 5",
+    Description = "Hotel with sea view rooms",
+    Rating = 4.8m
+});
+var roomsService = new RoomsService();
+
+await roomsService.CreateRoomAsync(new Room
+{
+    HotelId = 1, 
+    TypeId = 1,  
+    RoomNumber = "101",
+    IsAvailable = true
+});
+
+await roomsService.CreateRoomAsync(new Room
+{
+    HotelId = 1, 
+    TypeId = 2,  
+    RoomNumber = "102",
+    IsAvailable = true
+});
