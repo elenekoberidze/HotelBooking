0f96e1fbc30507a98e0832c186264a25a116a9e8
koberidzeelene <koberidzeelene80@gmail.com>
2026-01-13 22:41:21 +0400

    .

--- HotelBooking/Interfaces/IRoomsService.cs
+++ HotelBooking/Interfaces/IRoomsService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelBooking.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,35 @@ namespace HotelBooking.Interfaces
 {
     internal interface IRoomsService
     {
+        /// <summary>
+        /// This Method adds new room.
+        /// </summary>
+        /// <param name="room">New Room.</param>
+        /// <returns></returns>
+        Task CreateRoomAsync(Room room);
+        /// <summary>
+        ///  /// This method updates remaining room.
+        /// </summary>
+        /// <param name="id">Room id.</param>
+        /// <param name="isAvailable"></param>
+        /// <returns></returns>
+        Task UpdateRoomAvailabilityAsync(int id, bool isAvailable);
+        /// <summary>
+        ///This method deletes rooms.
+        /// </summary>
+        /// <param name="id">Room id.</param>
+        /// <returns></returns>
+        Task DeleteRoomAsync(int id);
+        /// <summary>
+        /// This method returns rooms.
+        /// </summary>
+        /// <returns>All Rooms</returns>
+        Task<ICollection<Room>> GetRoomsAsync();
+        /// <summary>
+        /// This method returns rooms by id.
+        /// </summary>
+        /// <param name="id">Room id.</param>
+        /// <returns>Room</returns>
+        Task<Room?> GetRoomByIdAsync(int id);
     }
 }
--- HotelBooking/Services/RoomsService.cs
+++ HotelBooking/Services/RoomsService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HotelBooking.Data;
+using HotelBooking.Interfaces;
+using HotelBooking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,51 @@ using System.Threading.Tasks;
 
 namespace HotelBooking.Services
 {
-    internal class RoomsService
+    internal class RoomsService : IRoomsService
     {
+        private readonly HotelBookingContext hotelBookingContext;
+        public RoomsService()
+        {
+            hotelBookingContext = new HotelBookingContext();
+        }
+        /// <inheritdoc/>
+        public async Task CreateRoomAsync(Room room)
+        {
+            await hotelBookingContext.Rooms.AddAsync(room);
+            await hotelBookingContext.SaveChangesAsync();
+        }
+        /// <inheritdoc/>
+        public async Task DeleteRoomAsync(int id)
+        {
+            var room = await hotelBookingContext.Rooms.FindAsync(id);
+            if (room == null) { return; }
+
+            hotelBookingContext.Rooms.Remove(room);
+            await hotelBookingContext.SaveChangesAsync();
+        }
+        /// <inheritdoc/>
+        public async Task<Room?> GetRoomByIdAsync(int id)
+        {
+            return await hotelBookingContext.Rooms
+            .Include(r => r.RoomImages)
+            .FirstOrDefaultAsync(r => r.RoomId == id);
+        }
+        /// <inheritdoc/>
+        public async Task<ICollection<Room>> GetRoomsAsync()
+        {
+            return await hotelBookingContext.Rooms
+            .Include(r => r.Hotel)
+            .Include(r => r.Type)
+            .ToListAsync();
+        }
+        /// <inheritdoc/>
+        public async Task UpdateRoomAvailabilityAsync(int id, bool isAvailable)
+        {
+            var room = await hotelBookingContext.Rooms.FindAsync(id);
+            if (room == null) { return; }
+
+            room.IsAvailable = isAvailable;
+            await hotelBookingContext.SaveChangesAsync();
+        }
     }
 }
