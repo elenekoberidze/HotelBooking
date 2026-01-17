4b81b09501aa41020c99b50422377d26f2b72950
koberidzeelene <koberidzeelene80@gmail.com>
2026-01-18 00:10:52 +0400

    .

--- HotelBooking/Interfaces/IBookingsService.cs
+++ HotelBooking/Interfaces/IBookingsService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HotelBooking.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,45 @@ using System.Threading.Tasks;
 
 namespace HotelBooking.Interfaces
 {
-    internal interface IBookingsService
+
+    
+    public interface IBookingsService
     {
+        /// <summary>
+        /// Adds a new booking.
+        /// </summary>
+        /// <param name="booking">New Booking details.</param>
+        /// <returns></returns>
+        Task CreateBookingAsync(Booking booking);
+
+        /// <summary>
+        /// Updates the dates for an existing booking.
+        /// </summary>
+        /// <param name="id">Booking ID.</param>
+        /// <param name="checkIn">New Check-in date.</param>
+        /// <param name="checkOut">New Check-out date.</param>
+        /// <returns></returns>
+        Task UpdateBookingDatesAsync(int id, DateOnly checkIn, DateOnly checkOut);
+
+        /// <summary>
+        /// Deletes a booking record.
+        /// </summary>
+        /// <param name="id">Booking ID.</param>
+        /// <returns></returns>
+        Task DeleteBookingAsync(int id);
+
+        /// <summary>
+        /// Returns all bookings with Room and User details.
+        /// </summary>
+        /// <returns>A collection of all bookings.</returns>
+        Task<ICollection<Booking>> GetBookingsAsync();
+
+        /// <summary>
+        /// Returns a specific booking by its ID.
+        /// </summary>
+        /// <param name="id">Booking ID.</param>
+        /// <returns>Booking details or null if not found.</returns>
+        Task<Booking?> GetBookingByIdAsync(int id);
     }
 }
+
--- HotelBooking/Services/BookingsService.cs
+++ HotelBooking/Services/BookingsService.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using HotelBooking.Data;
+using HotelBooking.Interfaces;
+using HotelBooking.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,63 @@ using System.Threading.Tasks;
 
 namespace HotelBooking.Services
 {
-    internal class BookingsService
+    internal class BookingsService : IBookingsService
     {
+        private readonly HotelBookingContext hotelBookingContext;
+
+        public BookingsService()
+        {
+            hotelBookingContext = new HotelBookingContext();
+        }
+
+        /// <inheritdoc/>
+        public async Task CreateBookingAsync(Booking booking)
+        {
+            
+            booking.CreatedAt ??= DateTime.Now;
+
+            await hotelBookingContext.Bookings.AddAsync(booking);
+            await hotelBookingContext.SaveChangesAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task DeleteBookingAsync(int id)
+        {
+            var booking = await hotelBookingContext.Bookings.FindAsync(id);
+            if (booking == null) { return; }
+
+            hotelBookingContext.Bookings.Remove(booking);
+            await hotelBookingContext.SaveChangesAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task<Booking?> GetBookingByIdAsync(int id)
+        {
+            return await hotelBookingContext.Bookings
+                .Include(b => b.Room)
+                .Include(b => b.User)
+                .FirstOrDefaultAsync(b => b.BookingId == id);
+        }
+
+        /// <inheritdoc/>
+        public async Task<ICollection<Booking>> GetBookingsAsync()
+        {
+            return await hotelBookingContext.Bookings
+                .Include(b => b.Room)
+                .Include(b => b.User)
+                .ToListAsync();
+        }
+
+        /// <inheritdoc/>
+        public async Task UpdateBookingDatesAsync(int id, DateOnly checkIn, DateOnly checkOut)
+        {
+            var booking = await hotelBookingContext.Bookings.FindAsync(id);
+            if (booking == null) { return; }
+
+            booking.CheckInDate = checkIn;
+            booking.CheckOutDate = checkOut;
+
+            await hotelBookingContext.SaveChangesAsync();
+        }
     }
 }
